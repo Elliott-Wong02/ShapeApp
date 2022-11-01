502c837900fac5aa10ff87e9ef7555eb51b592d1
Elliott-Wong02 <ekw120@ic.ac.uk>
2022-11-01 18:26:01 +0000

    v1

--- src/main/java/drawingUI/DrawingUIController.java
+++ src/main/java/drawingUI/DrawingUIController.java
@@ -1,2 +1,58 @@
-package drawingUI;public class DrawingUIController {
+package drawingUI;
+
+import shapes.Drawing;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Objects;
+
+public class DrawingUIController {
+    private JPanel controlsPanel = new JPanel(new GridLayout(3,1));
+    private JPanel mainPanel = new JPanel(new GridLayout(2,1));
+    public DrawingUIController(){
+        ShapeSelector sSel = new ShapeSelector();
+        controlsPanel.add(sSel);
+        Drawing d = new Drawing();
+        d.addMouseListener(new MouseListener(){
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (Objects.equals(sSel.getCurrentShape(), "Circle")) {
+                    d.addCircle(e.getPoint(), new Color(0x992266), 100);
+                    d.repaint();
+                } else if (Objects.equals(sSel.getCurrentShape(), "Rectangle")) {
+                    d.addRect(e.getPoint(), new Color(29, 100, 57), 100, 300);
+                    d.repaint();
+                } else if (Objects.equals(sSel.getCurrentShape(), "Square")) {
+                    d.addSquare(e.getPoint(), new Color(39, 23, 245), 50);
+                    d.repaint();
+                }
+            }
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+        mainPanel.add(controlsPanel);
+        mainPanel.add(d);
+    }
+    public JPanel getMainPanel(){
+        return mainPanel;
+    }
 }
--- src/main/java/shapes/ShapeDB.java
+++ src/main/java/shapes/ShapeDB.java
@@ -1,2 +1,23 @@
-package shapes;public class ShapeDB {
+package shapes;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class ShapeDB {
+    public ArrayList<Shape> shapes = new ArrayList<>();
+    void addCircle(Point initpos, Color col, int radius){
+        shapes.add(new Circle(initpos, col, radius));
+    }
+    void addRect(Point initpos, Color col, int w, int h){
+        shapes.add(new Rect(initpos, col, w, h));
+    }
+    void addSquare(Point initpos, Color col, int side){
+        shapes.add(new Square(initpos, col, side));
+    }
+    void drawShapes(Graphics g) {
+        for (int i = 0; i < shapes.size(); i++) {
+            shapes.get(i).draw(g);
+        }
+    }
+
 }
